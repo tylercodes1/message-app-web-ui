0eb2acaf46c1f563f65b3b8067544b7affdb2173
tkmoney11 <tylerkim@outlook.com>
2021-02-14 19:56:45 -0800

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import ChatroomPage from './Pages/ChatroomPage/ChatroomPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Yohama from './Pages/Yohama/Yohama'
 
 function App() {
-   return <ChatroomPage />
+   return (
+      <Router>
+         <Switch>
+            <Route path="/" exact component={ChatroomPage} />
+            <Route path="/yohammma" component={Yohama} />
+         </Switch>
+      </Router>
+   )
 }
 
 export default App
